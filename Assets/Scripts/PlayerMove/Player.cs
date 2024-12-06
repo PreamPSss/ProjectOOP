f14801808decf3e9816272414d015269268f10ef
PreamPSss <pattapon.sael@bumail.net>
2024-12-06 14:22:44 +0700

    player walk

---
+++ Assets/Scripts/PlayerMove/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7.5f;
+    
+    private Rigidbody2D rb;
+    private float moveInput;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        moveInput = UserInput.instance.moveInput.x;
+
+        rb.velocity = new Vector2 (moveInput * moveSpeed, rb.velocity.y);
+
+    }
+}
