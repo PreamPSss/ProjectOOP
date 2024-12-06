268b9fd9437d9f6bdb99769ba4f0529e74111fdf
PreamPSss <pattapon.sael@bumail.net>
2024-12-06 18:55:13 +0700

    Shoot

--- Assets/Scripts/GhostWalker.cs
+++ Assets/Scripts/GhostWalker.cs
@@ -4,43 +4,42 @@ using UnityEngine;
 
 public class GhostWalker : Enemy
 {
-   
-    
-        private void Start()
-        {
-            //rb = GetComponent <Rigidbody2D>();
+    [SerializeField] private Vector2 velocity;
+    [SerializeField] private Transform[] movePoints;
 
-        }
-        private void FlipCharater()
-        {
-            velocity *= -1;
+    private void Start()
+    {
+        //rb = GetComponent <Rigidbody2D>();
 
-            Vector2 scale = transform.localScale;
-            scale.x *= -1;
-            transform.localScale = scale;
-        }
-
-
-        [SerializeField] private Vector2 velocity;
-        [SerializeField] private Transform[] movePoints; 
-
-        private void FixedUpdate()
-        {
-            Behavior();
-        }
-
-        public override void Behavior()
-        {
-            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
-            if (rb.position.x <= movePoints[0].position.x && velocity.x < 0)
-            {
-                FlipCharater();
-
-            }
-            else if (rb.position.x >= movePoints[1].position.x && velocity.x > 0)
-            {
-                FlipCharater();
-            }
-
-        }
     }
+    private void FlipCharater()
+    {
+        velocity *= -1;
+
+        Vector2 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+
+    private void FixedUpdate()
+    {
+        Behavior();
+    }
+
+    public override void Behavior()
+    {
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        if (rb.position.x <= movePoints[0].position.x && velocity.x < 0)
+        {
+            FlipCharater();
+
+        }
+        else if (rb.position.x >= movePoints[1].position.x && velocity.x > 0)
+        {
+            FlipCharater();
+        }
+
+    }
+}
+
