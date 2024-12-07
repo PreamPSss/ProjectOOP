e28e75a8c27d5029f3f87a32d11a33bb4f5d5860
PreamPSss <pattapon.sael@bumail.net>
2024-12-07 12:54:48 +0700

    Boomdamage

--- Assets/Scripts/BoomScript.cs
+++ Assets/Scripts/BoomScript.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoomScript : MonoBehaviour
+public class BoomScript : Weapon
 {
     private GameObject player;
     private Rigidbody2D rb;
     public float force;
 
-    private void Start()
+
+    public override void Move()
     {
-        rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
 
         Vector3 direction = player.transform.position - transform.position;
-        rb.velocity = new Vector2 (direction.x, direction.y).normalized * force;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+    }
+    public override void OnHitWith(Character character)
+    {
+        if (character is Player)
+            character.TakeDamage(this.Damage);
+
+    }
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Move();
     }
 }
